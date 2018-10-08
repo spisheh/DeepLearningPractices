57900119d348bcb1b3d8b795e20b2a13b5c2e176
Sam Pisheh <spisheh@berkeley.edu>
2018-10-08 00:43:13 -0700

    second week

---
+++ numpy/tools.py
@@ -0,0 +1,57 @@
+def softmax(x):
+    """Calculates the softmax for each row of the input x.
+    Workd for a row vector and also for matrices of shape (n, m).
+    Argument:
+    x -- A numpy matrix of shape (n,m)
+
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (n,m)
+    """
+    x_exp = np.exp(x)
+    x_sum = np.sum(x_exp,axis=1, keepdims = True)
+    
+    return s
+
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """    
+    loss = np.sum(abs(y-yhat),axis=0)
+    
+    return loss
+
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    loss = np.sum((y-yhat)**2,axis=0)
+    
+    return loss
+
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s=np.exp(-x)+1
+    s = 1/s
+    
+    return s
--- numpy/vector.py
+++ numpy/vector.py
@@ -1,7 +1,7 @@
 import numpy as np 
 from time import time
 
-a=np.random.rand(10000000)
+a=np.random.rand(10000000) #rank 1 array
 b=np.random.rand(10000000)
 
 t0=time()
@@ -17,3 +17,19 @@ for i in range(10000000):
 
 print("for loop time:", format(time()-t1, '.5g'), "s")
 print("the result of ab=",format(ab, '.2g'), "is equal to loop=", format(loop, '.3g'))
+
+
+# built in functions 
+"""
+v = vector
+np.exp(v)
+np.log(v)
+np.abs(v)
+np.maximum(v,0)
+np.sum(x,axis=0)  sum vertically (cols)
+np.sum(x,axis=1)  sum horizontally (rows)
+v**2
+1/v
+v.reshape(row,col)
+x_norm = np.linalg.norm(x,axis=1,keepdims=True)
+"""
